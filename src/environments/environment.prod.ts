bdcf1e920491e17501be15868d9062e69dfd38c2
Chinh Nguyen Dong <nguyendongminhtri29092014@gmail.com>
2020-12-08 01:45:33 +0700

    form regsiter

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_LOCAL: 'http://localhost:8080/api/auth/',
-  API_SERVER: 'https://back-end-music.herokuapp.com/',
+  API_SERVER: 'https://back-end-music.herokuapp.com/api/auth',
 };
